95ff6f059e07344a1432def2d7fd48c592ca72e2
adityaval317 <adityavaliveti317@gmail.com>
2025-06-06 03:24:21 -0400

    Config Server

---
+++ config-server/src/main/java/com/aditya/microservices/spring/cloud/config_server/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.aditya.microservices.spring.cloud.config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
