1f58cfddb266fc409531e3bdf4812302806d8930
adityaval317 <adityavaliveti317@gmail.com>
2025-06-07 09:51:44 -0400

    Config update

--- cards/src/main/java/com/aditya/microservices/cards/dto/ContactInfoDetailsDto.java
+++ cards/src/main/java/com/aditya/microservices/cards/dto/ContactInfoDetailsDto.java
@@ -1,12 +1,27 @@
 package com.aditya.microservices.cards.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 
-@ConfigurationProperties(prefix = "loans")
-public record ContactInfoDetailsDto(String message, ContactDetails contactDetails, OnCallSupport onCallSupport) {
-    public record ContactDetails(String name, String email) {}
-    public record OnCallSupport(String name, List<String> phone) {}
+@ConfigurationProperties(prefix = "cards")
+@Getter @Setter
+public class ContactInfoDetailsDto{
+    private String message;
+    private ContactDetails contactDetails;
+    private OnCallSupport onCallSupport;
+}
 
+@Getter @Setter
+class ContactDetails {
+    private String name;
+    private String email;
+}
+
+@Getter @Setter
+class OnCallSupport {
+    private String name;
+    private List<String> phone;
 }
